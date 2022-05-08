61d5f8730935b110578793655c8476979cedcc90
Mayuresh <mayuresh365@gmail.com>
2022-05-08 14:41:36 +0530

    second commit from another guy

--- src/MockJava/Jan19.java
+++ src/MockJava/Jan19.java
@@ -5,6 +5,9 @@ public class Jan19 {
 	public static void main(String[] args) {
 
 		System.out.println("Mayuresh");
+		System.out.println("Mayuresh");
+		System.out.println("Mayuresh");
+		System.out.println("Mayuresh");
 	}
 
 }
